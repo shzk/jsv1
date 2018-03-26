eda6c3970d9354a4c5fb4f05178e1e1c61746a18
Denis <den4@me.com>
2018-03-26 21:19:06 +0300

    1st lesson done

---
+++ src/js/script.js
@@ -0,0 +1,24 @@
+let budget,
+    storeName;
+
+budget = prompt('Ваш бюджет?');
+storeName = prompt('Название вашего магазина?');
+
+let shopGoods = [];
+
+for (let i = 0; i < 3; i++ ) {
+    shopGoods.push(prompt('Какой тип товаров будем продавать?'));
+};
+
+mainList = {
+        'budget': budget,
+        'storeName': storeName,
+        'shopGoods': shopGoods,
+        'employers': {'employer1' : 'Vasya', 'employer2' : 'Petya', 'employer3' : 'Masha'},
+        'open': function(){ console.log('hi from open method!'); }
+    };
+console.log(mainList);
+console.dir( 'mainList object = ' + JSON.stringify(mainList, null, 4)); /* printable version of mainList object - but there is no open method - why? */
+
+let dayBudget = mainList.budget/30;
+document.getElementById("budget").innerHTML = dayBudget;
