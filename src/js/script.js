96da1399a36494ecab54671223d509a21af93bca
Denis <den4@me.com>
2018-03-26 21:20:34 +0300

    1st lesson advanced done

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,27 @@
-let budget,
-    storeName;
+document.addEventListener("DOMContentLoaded", function(event) { 
 
-budget = prompt('Ваш бюджет?');
-storeName = prompt('Название вашего магазина?');
+    let budget,
+        storeName;
 
-let shopGoods = [];
+    budget = prompt('Ваш бюджет?');
+    storeName = prompt('Название вашего магазина?');
 
-for (let i = 0; i < 3; i++ ) {
-    shopGoods.push(prompt('Какой тип товаров будем продавать?'));
-};
+    let shopGoods = [];
 
-mainList = {
-        'budget': budget,
-        'storeName': storeName,
-        'shopGoods': shopGoods,
-        'employers': {'employer1' : 'Vasya', 'employer2' : 'Petya', 'employer3' : 'Masha'},
-        'open': function(){ console.log('hi from open method!'); }
+    for (let i = 0; i < 3; i++ ) {
+        shopGoods.push(prompt('Какой тип товаров будем продавать?'));
     };
-console.log(mainList);
-console.dir( 'mainList object = ' + JSON.stringify(mainList, null, 4)); /* printable version of mainList object - but there is no open method - why? */
 
-let dayBudget = mainList.budget/30;
-document.getElementById("budget").innerHTML = dayBudget;
+    mainList = {
+            'budget': budget,
+            'storeName': storeName,
+            'shopGoods': shopGoods,
+            'employers': {'employer1' : 'Vasya', 'employer2' : 'Petya', 'employer3' : 'Masha'},
+            'open': true
+        };
+    console.log(mainList);
+    console.dir( 'mainList object = ' + JSON.stringify(mainList, null, 4)); /* printable version of mainList object - but there is no open method - why? */
+
+    let dayBudget = mainList.budget/30;
+    document.getElementById("budget").innerHTML = dayBudget;
+});
