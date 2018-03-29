480e7688ba88e37ef371fe1ee0726d21d390567d
Denis <den4@me.com>
2018-03-29 09:39:10 +0300

    2nd lesson

--- src/js/script.js
+++ src/js/script.js
@@ -1,26 +1,61 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
 
     let budget,
-        storeName;
+        storeName,
+        time = 19;
 
     budget = prompt('Ваш бюджет?');
-    storeName = prompt('Название вашего магазина?');
-
-    let shopGoods = [];
-
-    for (let i = 0; i < 3; i++ ) {
-        shopGoods.push(prompt('Какой тип товаров будем продавать?'));
-    };
+    storeName = prompt('Название вашего магазина?');  
 
     mainList = {
             'budget': budget,
             'storeName': storeName,
-            'shopGoods': shopGoods,
+            'shopGoods': [],
             'employers': {'employer1' : 'Vasya', 'employer2' : 'Petya', 'employer3' : 'Masha'},
             'open': true
         };
+
+    //initial solution
+    /*for (let i = 0; i < 3; i++ ) {
+        mainList.shopGoods.push(prompt('Какой тип товаров будем продавать?'));
+    };*/
+
+    //1st cycle method
+    /*let i = 0;
+    while (i < 3) {
+        mainList.shopGoods[i] = prompt('Какой тип товаров будем продавать?');
+        i++;
+    };*/
+
+    //2nd cycle method
+    /*let i = 0;
+    do {
+        mainList.shopGoods[i] = prompt('Какой тип товаров будем продавать?');
+        i++;
+    } while (i < 3);*/
+
+    //3rd cycle method
+    for (let i = 0; i < 3; i++ ) {
+        let a = prompt('Какой тип товаров будем продавать?');
+        
+        if ((typeof(a)) === 'string' && (typeof(a)) !== null && a != '' && a.length < 50) {
+            console.log('ok!');
+            mainList.shopGoods[i] = a;
+        }
+    };
+    
+    if (time < 0) {
+        console.log('This can\'t be!');
+    } else if(time > 8 && time < 20) {
+            console.log('It\'s working time :)');
+        } else if (time < 24) {
+                console.log('It\'s too late :(');
+            } else {
+                console.log('Something is wrong, there is only 24hrs in a day!');
+            }
+
     console.log(mainList);
-    console.dir( 'mainList object = ' + JSON.stringify(mainList, null, 4)); /* printable version of mainList object - but there is no open method - why? */
+    console.dir( 'mainList object = ' + JSON.stringify(mainList, null, 4)); /* printable version of mainList object */
 
     let dayBudget = mainList.budget/30;
     document.getElementById("budget").innerHTML = dayBudget;
